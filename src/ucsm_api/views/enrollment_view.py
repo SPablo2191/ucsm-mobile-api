7b62573b33e500a4c5c48dcea8ad45fa198763ac
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-05-07 15:59:53 -0300

    fix: use params instead of body in get

--- src/ucsm_api/views/enrollment_view.py
+++ src/ucsm_api/views/enrollment_view.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
-from drf_spectacular.utils import extend_schema, inline_serializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, status, serializers
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
@@ -13,6 +13,9 @@ from ucsm_api.serializers.enrollment_serializer import EnrollmentSerializer
 
 @extend_schema(
     tags=[TagEnum.ENROLLMENT.value],
+    parameters=[
+            OpenApiParameter(name='identification_document', description='DNI', type=str),
+        ]
 )
 class EnrollmentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Enrollment.objects.all()
@@ -23,7 +26,7 @@ class EnrollmentViewSet(viewsets.ReadOnlyModelViewSet):
 
     def list(self, request):
         try:
-            identification_document = request.data.get("identification_document")
+            identification_document = request.query_params.get("identification_document")
             student = Student.objects.get(
                 identification_document=identification_document
             )
