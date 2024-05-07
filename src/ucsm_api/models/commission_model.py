9762e4d00bcf2bd0172d43bfe7de8c8e99f65b24
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-05-06 23:13:07 -0300

    fix: add commission field in model

--- src/ucsm_api/models/commission_model.py
+++ src/ucsm_api/models/commission_model.py
@@ -7,19 +7,26 @@ from ucsm_api.models.classroom_model import Classroom
 from ucsm_api.models.commission_schedule_model import CommissionSchedule
 from ucsm_api.models.professor_model import Professor
 
+
+class Commission(models.TextChoices):
+    THEORY = "Teoría"
+    PRACTICE = "Práctica"
+
+
 class Commission(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    commission_type = models.CharField(max_length=20, default=Commission.THEORY.value)
     commission_schedule = models.OneToOneField(CommissionSchedule, on_delete=models.CASCADE)
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, default=TableStatus.ACTIVE.value)
     register_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.professor} - {self.classroom} - {self.commission_schedule}"
+        return f"{self.professor} - {self.classroom} - {self.commission_type}"
 
 @admin.register(Commission)
 class CommissionAdmin(admin.ModelAdmin):
-    list_display = ("id","professor","classroom","commission_schedule")
+    list_display = ("id","professor","classroom","commission_type","commission_schedule")
